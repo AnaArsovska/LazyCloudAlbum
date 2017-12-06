effbee03c0daabb3fe644fdc6abfcbdfa98f030f
Morgan <mrgnmcsmith@gmail.com>
2017-12-06 17:47:14 -0500

    Last minute changes

--- construction.py
+++ construction.py
@@ -62,7 +62,10 @@ class Construct(webapp2.RequestHandler):
         utils.upload_text_file_to_cloudstorage(filename, html)
         album.ready = True
         album.put()
-        #utils.send_album_email("mrgnmcsmith@gmail.com", "Album")
+        #try:
+        #    utils.send_album_email(account.user_id, "Album")
+        #except:
+        #    pass name, email, album_key
 
 class Delete(webapp2.RequestHandler):
     @ndb.transactional
